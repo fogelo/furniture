51bef8d20826001a905d5160afbf313c34fc8035
Орлов Антон <orlov@kvantprogramm.ru>
2023-03-05 23:00:56 +0300

    сделал top блок со слайдером

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function () {
+    $('.header__btn').on("click", function () {
+        $('.rightside-menu').removeClass('rightside-menu--close')
+        console.log($('rightside-menu'));
+    })
+    $('.rightside-menu--close').on('click', function () {
+        $('.rightside-menu').addClass('rightside-menu--close')
+    })
+    $('.top__slider').slick({ dots: true, arrows: false, autoplay: true})
 
 })
